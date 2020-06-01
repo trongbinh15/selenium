95c80539247f5be20b94f2836ee0aa058405bae0
Trong Binh Nguyen <trongbinhnguyen15@gmail.com>
2020-06-02 06:46:19 +0700

    combine sucess

--- action/login.test.js
+++ action/login.test.js
@@ -1,24 +1,27 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
 
-(async function example() {
-    let driver = await new Builder().forBrowser('chrome').build();
-    try {
-        await driver.get('http://localhost:4200/');
-        await driver.wait(until.elementLocated(By.css('h2')), 5000);
+const { Builder, By, Key, until } = require('selenium-webdriver');
 
-        await driver.findElement(By.name('Username')).sendKeys('administrator@gmail.com');
-        await driver.findElement(By.name('Password')).sendKeys('123456', Key.ENTER);
+let driver;
 
-        await driver.wait(until.elementLocated(By.xpath("//p[@class='list-view-title h4']")), 5000);
+beforeEach(async () => {
+    driver = await new Builder().forBrowser('chrome').build();
+});
 
-        const a = await driver.findElement(By.xpath("//p[@class='list-view-title h4']"))
+test('login PIM', async () => {
+    await driver.get('http://pim.app.dev.retaildds.net/');
+    await driver.wait(until.elementLocated(By.css('h2')), 5000);
 
-        console.log('a:', a.getAttribute('value'))
+    await driver.findElement(By.name('Username')).sendKeys('administrator@gmail.com');
+    await driver.findElement(By.name('Password')).sendKeys('123456', Key.ENTER);
 
-        await driver.wait(until.elementLocated(By.xpath('h33332')), 5000);
+    await driver.wait(until.elementLocated(By.xpath("//p[@class='list-view-title h4']")), 5000);
 
-    }
-    finally{
-        driver.quit();
-    }
-})();
+    const title = await driver.findElement(By.xpath("//p[@class='list-view-title h4']")).getText();
+
+    expect(title).toBe('Product Management')
+});
+
+
+afterEach(() => {
+    driver.quit();
+})
