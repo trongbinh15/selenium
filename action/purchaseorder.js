5322272218808627d98fc5c6778430bebc24c583
Trong Binh <trongbinhnguyen15@gmail.com>
2020-06-03 18:51:42 +0700

    input error

--- action/purchaseorder.js
+++ action/purchaseorder.js
@@ -12,9 +12,10 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
         const title = await (driver.wait(until.elementLocated(By.xpath("//p[@class='list-view-title h4']")), 5000)).getText();
 
         if(title ==='Purchase Order Management'){
-            (await driver.wait(until.elementLocated(By.xpath("//button[contains(text(), 'Add')]")), 5000)).click();
-            // (await driver.findElement(By.xpath("//button[contains(text(), 'Add')]"))).click();
+            (await driver.wait(until.elementLocated(By.xpath("//app-list-view/div[3]/div[1]/button")), 5000)).click();
+            driver.sleep(1000);
             await (driver.wait(until.elementLocated(By.xpath("//*[@class='modal-title']")), 5000)).getText();
+
             await driver.findElement(By.xpath("//input[@formcontrolname='name']")).sendKeys('PO1');
             await driver.findElement(By.xpath("//input[@formcontrolname='description']")).sendKeys('PO1');
 
@@ -22,10 +23,33 @@ const { Builder, By, Key, until } = require('selenium-webdriver');
             const options = (await driver.wait(until.elementsLocated(By.xpath("//div[@class='ng-option']")), 500));
             options[0].click();
             await driver.sleep(1000);
-            const a = await (await driver.findElement(By.xpath("//div[@class='datatable']/*//button"))).click();
+            const addButton =  await driver.findElement(By.xpath("//div[@class='modal-content']/div[2]/div/div[1]/div[1]/button"));
+
+            addButton.click();
+            await driver.sleep(1000);
+            await driver.findElement(By.xpath("//table/tbody/tr[2]//th[1]/div/ng-select/div/div/div[2]/input")).sendKeys('GM10320001906EE1');
+            await (await driver.findElement(By.xpath("//table/tbody/tr[2]/th[3]/div"))).click();
+            await driver.sleep(1000);
+            await (await driver.findElement(By.xpath("//table/tbody/tr[2]/*//ng-dropdown-panel/div/div[2]/div[1]"))).click();
+            await driver.findElement(By.xpath("//table/tbody/tr[2]//th[4]/div/input")).sendKeys('10');
+            await driver.findElement(By.xpath("//table/tbody/tr[2]//th[5]/div/input")).sendKeys('10');
+            (await driver.findElement(By.xpath("//app-summary/div"))).click();
+
+            await driver.sleep(1000);
+
+            addButton.click();
+            await driver.sleep(1000);
+            await driver.findElement(By.xpath("//table/tbody/tr[3]//th[1]/div/ng-select/div/div/div[2]/input")).sendKeys('GM10320001906EE1');
+            await driver.findElement(By.xpath("//table/tbody/tr[3]//th[4]/div/input")).sendKeys('10');
+            await driver.findElement(By.xpath("//table/tbody/tr[3]//th[5]/div/input")).sendKeys('10');
+            await (await driver.findElement(By.xpath("//table/tbody/tr[3]/th[3]/div"))).click();
+            await driver.sleep(1000);
+            await (await driver.findElement(By.xpath("//table/tbody/tr[3]/*//ng-dropdown-panel/div/div[2]/div[1]/span"))).click();
+            await driver.sleep(1000);
+            (await driver.findElement(By.xpath("//app-summary/div"))).click();
         }
 
-        await driver.sleep(5000);
+        await driver.sleep(10000);
 
 
     }
---
+++ utils/getter.js
@@ -0,0 +1,9 @@
+
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+let driver = await new Builder().forBrowser('chrome').build();
+
+
+export function getElement (xpath){
+    
+};
